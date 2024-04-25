a294978818623857099d8084e0991b64fd842ad5
NathanNOVARESE <nathan.novarese@gmail.com>
2024-04-25 11:52:09 +0200

    FOND + PROJECTILES

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,25 @@ let vxl = 0;
 let vxr = 0;
 let vyu = 0;
 let vyd =0;
+
 function update(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    x += vxl ;
-    x += vxr ;
-    y += vyu ;
-    y += vyd ;
-    ctx.fillRect(x, y, 10, 10)
-    requestAnimationFrame(update)
+    x += vxl + vxr;
+    y += vyu + vyd;
+
+    // Mise à jour et dessin des projectiles
+    for (let i = 0; i < projectiles.length; i++) {
+        const projectile = projectiles[i];
+        projectile.update();
+        projectile.draw();
+    }
+
+    ctx.fillRect(x, y, 10, 10);
+    requestAnimationFrame(update);
 }
+
+
+
+const projectiles = []; // Tableau pour stocker les projectiles
+
 update()
--- scripts/mouvement.js
+++ scripts/mouvement.js
@@ -1,3 +1,22 @@
+class Projectile {
+    constructor(x, y, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.size = 5 ; // Taille du projectile
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    draw() {
+        ctx.fillStyle = "black"; // Couleur du projectile
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+}
 
 
 addEventListener("keydown", function(e){
@@ -5,6 +24,10 @@ addEventListener("keydown", function(e){
     if (e.code == 'KeyA') vxl = -1;
     if (e.code == 'KeyW') vyu = -1;
     if (e.code == 'KeyS') vyd = 1;
+    if (e.code == 'Space') { 
+        const projectile = new Projectile(x+2.5, y, 0, -2); 
+        projectiles.push(projectile); // Ajout du projectile au tableau
+    }
 })
 
 addEventListener("keyup", function(e){
@@ -12,4 +35,5 @@ addEventListener("keyup", function(e){
     if (e.code == 'KeyA') vxl = 0;
     if (e.code == 'KeyW') vyu = 0;
     if (e.code == 'KeyS') vyd = 0;
+
 })
