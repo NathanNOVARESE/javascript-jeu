cedc18676623a938c1dbc681f818041def086937
TomBallauri <144777722+TomBallauri@users.noreply.github.com>
2024-04-29 11:52:09 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,37 @@ console.log("Main.js loaded")
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
-let x = 43.5;
+let x = 100.5;
 let y = 125;
 let vxl = 0;
 let vxr = 0;
 let vyu = 0;
 let vyd =0;
+const characterWidth = 10;
+const characterHeight = 10;
+
 function update(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    x += vxl ;
-    x += vxr ;
-    y += vyu ;
-    y += vyd ;
-    ctx.fillRect(x, y, 10, 10)
-    requestAnimationFrame(update)
+    
+    x += vxl + vxr;
+    y += vyu + vyd;
+
+    if (x < 0) {
+        x = 0;
+    } else if (x + characterWidth > canvas.width) {
+        x = canvas.width - characterWidth;
+    }
+
+    if (y < 0) {
+        y = 0;
+    } else if (y + characterHeight > canvas.height) {
+        y = canvas.height - characterHeight;
+    }
+
+    ctx.fillRect(x, y, characterWidth, characterHeight);
+
+    requestAnimationFrame(update);
 }
+
+
 update()
